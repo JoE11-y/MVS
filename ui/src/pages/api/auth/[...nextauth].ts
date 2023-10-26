630384885dab02a9fefdc644f1b44e3530354155
JoE11-y <josephedoh77@gmail.com>
2023-10-26 06:15:26 +0100

    (auth-setup): setup next-auth for github authentication

---
+++ ui/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+};
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, options);
+}
