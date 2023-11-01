5ae9d14a2be3cd17a2439f039973cb04b0eacb8f
JoE11-y <josephedoh77@gmail.com>
2023-11-01 08:34:36 +0100

    restructure directory to use workspaces

---
+++ ui/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import Layout from "../components/Layout";
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
+}
