867e75ee4d3c72cce2f3d5a2e2a00611eb571bcf
JoE11-y <josephedoh77@gmail.com>
2023-11-11 07:18:57 +0100

    clean imports

--- ui/src/pages/demo/authenticate.tsx
+++ ui/src/pages/demo/authenticate.tsx
@@ -9,24 +9,6 @@ import { useSession } from "next-auth/react"
 export default function Authenticate({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     const { data: session, status } = useSession();
     const loading = status === "unauthenticated";
-
-    useEffect(() => {
-        (async () => {
-            const { Mina, PublicKey } = (await import("o1js"));
-            const { Add } = await import('../../../../contracts/build/contracts/src');
-            // // Update this to use the address (public key) for your zkApp account.
-            // // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
-            // // Berkeley Testnet .
-            const zkAppAddress = "B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA";
-            // // This should be removed once the zkAppAddress is updated.
-            // if (!zkAppAddress) {
-            //   console.error(
-            //     'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
-            //   );
-            // }
-            const zkApp = new Add(PublicKey.fromBase58(zkAppAddress));
-        })();
-    }, []);
     return (
         <main className={styles.main}>
             <h2 className={styles.title}>AUTHENTICATE</h2>
